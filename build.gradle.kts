57bc751c72169f099bb12b9f7d038cc46a468b1a
unknown <nikolinarogan67@gmail.com>
2025-08-27 13:33:19 +0200

    crush: firestore doesnt work

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt.android.gradle.plugin) apply false
-    //alias(libs.plugins.google.gms.google.services) apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    alias(libs.plugins.google.gms.google.services)  apply false
+    //id("com.google.gms.google-services") version "4.3.15" apply false
 }
